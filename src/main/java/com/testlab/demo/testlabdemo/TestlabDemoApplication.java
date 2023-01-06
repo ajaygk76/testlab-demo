c6dc138d88d3ca7599545ae5b028f11de3375d25
ajaygk76 <ajaygk@gmail.com>
2023-01-06 13:26:57 +0530

    my first commit

---
+++ src/main/java/com/testlab/demo/testlabdemo/TestlabDemoApplication.java
@@ -0,0 +1,13 @@
+package com.testlab.demo.testlabdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestlabDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestlabDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testlab/demo/testlabdemo/TestlabDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testlab.demo.testlabdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestlabDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
